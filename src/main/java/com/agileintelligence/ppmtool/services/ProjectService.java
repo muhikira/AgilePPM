d927163a2997c7cf14586ed86c996881b61a6cb8
muhikira <muhikiraismael@gmail.com>
2022-12-28 13:15:10 -0600

    12/28/2022 commit1

---
+++ src/main/java/com/agileintelligence/ppmtool/services/ProjectService.java
@@ -0,0 +1,25 @@
+package com.agileintelligence.ppmtool.services;
+
+import com.agileintelligence.ppmtool.domain.Project;
+import com.agileintelligence.ppmtool.exceptions.ProjectIdException;
+import com.agileintelligence.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project) {
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
+
+    }
+
+}
